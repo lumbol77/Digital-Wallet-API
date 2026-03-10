c3f08a586d1be4ff8f0ac04919a6032baa676217
Lawrence <lumbolt@gmail.com>
2026-03-10 14:10:17 +0100

    Fix: define DATABASE_URL from environment

--- app/database.py
+++ app/database.py
@@ -1,16 +1,33 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
+#looks for .env file 
+load_dotenv()
+
+# grabs the DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# stops the app if the URL is missing
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set. Check your Render Environment Variables!")
+
+# Create the connection to database
 engine = create_engine(DATABASE_URL)
 
+# Create the SessionLocal class
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+# Create the Base class for models
 Base = declarative_base()
 
+# used routes to talk to the database
 def get_db():
     db = SessionLocal()
     try:
